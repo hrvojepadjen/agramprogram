0bb26590a0c676c4623e59c825bea260f3a9fd94
hrvoje <hrpadjen@gmail.com>
2024-11-22 23:51:49 +0100

    added search functionality

--- events/views.py
+++ events/views.py
@@ -1,5 +1,6 @@
 # django imports
 from django.shortcuts import render
+from django.db.models import Q
 
 # local imports
 from .models import Event
@@ -8,6 +9,13 @@ from .models import Event
 def landing_page(request):
     """View for the landing page of the events app.
     """
-    events = Event.objects.all()
+    query = request.GET.get('q')
 
-    return render(request, "landing_page.html", {"events": events})
+    if query:
+        events = Event.objects.filter(
+            Q(name__icontains=query) | Q(description__icontains=query)
+        )
+    else:
+        events = Event.objects.all()
+
+    return render(request, "landing_page.html", {"events": events, "query": query})
