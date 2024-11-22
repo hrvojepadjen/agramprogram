3bc30412ceb3b384db3782c0063ffe04369b1d63
hrvoje <hrpadjen@gmail.com>
2024-11-22 22:07:52 +0100

    added model for event type

--- events/models.py
+++ events/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class EventType(models.Model):
+    """
+    Represents the type of an event (e.g., Workshop, Play, Concert).
+    """
+    name = models.CharField(
+        max_length=255,
+        verbose_name=_("Event Type Name"),
+        help_text=_("The name of the event type.")
+    )
+
+    class Meta:
+        verbose_name = _("Event Type")
+        verbose_name_plural = _("Event Types")
+
+    def __str__(self):
+        return self.name
