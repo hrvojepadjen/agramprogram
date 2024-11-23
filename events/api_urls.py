3ce65b2a338a368215c09355f86b79c938baad3b
hrvoje <hrpadjen@gmail.com>
2024-11-23 16:40:07 +0100

    added endpoint for carousel data

--- events/api_urls.py
+++ events/api_urls.py
@@ -1,5 +1,17 @@
+# django imports
+from django.urls import path
+
+# 3rd party imports
 from rest_framework.routers import DefaultRouter
-from .api_views import EventViewSet, EventTypeViewSet, CategoryViewSet, AgeGroupViewSet
+
+# local imports
+from .api_views import (
+    EventViewSet,
+    EventTypeViewSet,
+    CategoryViewSet,
+    AgeGroupViewSet,
+    CarouselAPIView,
+)
 
 router = DefaultRouter()
 router.register(r'event-types', EventTypeViewSet, basename='event-type')
@@ -8,3 +20,7 @@ router.register(r'age-groups', AgeGroupViewSet, basename='age-group')
 router.register(r'events', EventViewSet, basename='event')
 
 urlpatterns = router.urls
+
+urlpatterns = router.urls + [
+    path('carousel/', CarouselAPIView.as_view(), name='blank-api'),  # Add the CarouselAPIView
+]
--- events/api_views.py
+++ events/api_views.py
@@ -1,11 +1,19 @@
+# django imports
+from django.utils.timezone import now
+
 # 3rd party imports
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+
+# python imports
+from datetime import timedelta
 
 # local imports
 from .models import Event, EventType, Category, AgeGroup
 from .serializers import EventSerializer, EventTypeSerializer, CategorySerializer, AgeGroupSerializer
+
 
 class EventTypeViewSet(viewsets.ReadOnlyModelViewSet):
     """
@@ -47,3 +55,45 @@ class EventViewSet(viewsets.ReadOnlyModelViewSet):
         'event_type__name',
         'categories__name',
     ]
+
+
+class CarouselAPIView(APIView):
+    """
+    An API view for carousel.
+    """
+    queryset = Event.objects.all()
+    allowed_methods = ['GET']
+
+    def get(self, request, *args, **kwargs):
+        # create data for carousel no. 1 - featured events
+        featured_events = self.queryset.filter(featured=True)
+        featured_serializer = EventSerializer(featured_events, many=True)
+
+        # create data for carousel no. 2 - today's events
+        today = now().date()
+        events_today = self.queryset.filter(
+            start_datetime__date=today
+        )
+        events_today_serializer = EventSerializer(events_today, many=True)
+
+        # create data for carousel no. 3 - tomorrow's events
+        tomorrow = now().date() + timedelta(days=1)
+        events_tomorrow = self.queryset.filter(
+            start_datetime__date=tomorrow
+        )
+        events_tomorrow_serializer = EventSerializer(
+            events_tomorrow,
+            many=True
+        )
+
+        # add data
+        data_total = {
+            "carousel_1_data": featured_serializer.data,
+            "carousel_2_data": events_today_serializer.data,
+            "carousel_3_data": events_tomorrow_serializer.data,
+        }
+
+        return Response(
+            data_total,
+            status=status.HTTP_200_OK
+        )
