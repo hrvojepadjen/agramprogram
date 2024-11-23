0f0b778c0ac55b0ac6aba3214513a5894436d2ca
hrvoje <hrpadjen@gmail.com>
2024-11-23 12:05:30 +0100

    api should be read only for this endpoints

--- organizers/api_views.py
+++ organizers/api_views.py
@@ -2,14 +2,14 @@ from rest_framework import viewsets
 from .models import Organizer, OrganizerType
 from .serializers import OrganizerSerializer, OrganizerTypeSerializer
 
-class OrganizerTypeViewSet(viewsets.ModelViewSet):
+class OrganizerTypeViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint for OrganizerType.
     """
     queryset = OrganizerType.objects.all()
     serializer_class = OrganizerTypeSerializer
 
-class OrganizerViewSet(viewsets.ModelViewSet):
+class OrganizerViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint for Organizer.
     """
